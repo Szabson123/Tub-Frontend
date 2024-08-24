d82959b81e60b6bb243d6916f736e2715a5c036b
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-08-24 10:14:09 +0200

    update 1

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -47,3 +47,18 @@ export class MainPageComponent implements OnInit {
     }
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show2');
+            } else {
+                entry.target.classList.remove('show2');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden2');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
